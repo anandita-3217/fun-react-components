77ed31cff0ac3f6665648cf4aa68c395e80468bd
Anandita <73057896+anandita-3217@users.noreply.github.com>
2026-07-16 14:46:13 +0530

    Update List.jsx

--- List.jsx
+++ List.jsx
@@ -19,25 +19,7 @@ const data = [
     author: 'Stanislaw Lem',
     year: 1961,
     reviews: { positive: 3487, negative: 1845 },
-  },
-  {
-    title: 'Dune',
-    author: 'Frank Herbert',
-    year: 1965,
-    reviews: { positive: 8576, negative: 663 },
-  },
-  {
-    title: 'The Left Hand of Darkness',
-    author: 'Ursula K. Le Guin',
-    year: 1969,
-    reviews: { positive: 6631, negative: 993 },
-  },
-  {
-    title: 'A Scanner Darkly',
-    author: 'Philip K Dick',
-    year: 1977,
-    reviews: { positive: 8124, negative: 1847 },
-  },
+  }
 ];
 
 export function Lisr() {
